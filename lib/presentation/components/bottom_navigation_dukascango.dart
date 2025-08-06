d8810a986d67dfd0d81b32c441940e4b3ccbc863
Alphonseaaron <alphonsemumbo@gmail.com>
2025-08-06 19:21:02 +0300

    Frave to DSG

--- lib/presentation/components/bottom_navigation_dukascango.dart
+++ lib/presentation/components/bottom_navigation_dukascango.dart
@@ -8,76 +8,79 @@ import 'package:dukascango/presentation/screens/client/search_client_screen.dart
 import 'package:dukascango/presentation/themes/colors_dukascango.dart';
 
 class BottomNavigationDukascango extends StatelessWidget {
-
   final int index;
 
   BottomNavigationDukascango(this.index);
 
   @override
-  Widget build(BuildContext context){
-    
+  Widget build(BuildContext context) {
     return Container(
-      height: 55,
-      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
+        height: 55,
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        decoration: BoxDecoration(color: Colors.white, boxShadow: [
           BoxShadow(color: Colors.grey, blurRadius: 10, spreadRadius: -5)
-        ]
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          _ItemButton(
-            i: 0, 
-            index: index, 
-            iconData: Icons.home_outlined, 
-            text: 'Home',
-            onPressed: () => Navigator.pushReplacement(context, routeDukascango(page: ClientHomeScreen())),
+        ]),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _ItemButton(
+              i: 0,
+              index: index,
+              iconData: Icons.home_outlined,
+              text: 'Home',
+              onPressed: () => Navigator.pushReplacement(
+                  context, routeDukascango(page: ClientHomeScreen())),
             ),
-          _ItemButton(
-            i: 1, 
-            index: index, 
-            iconData: Icons.search, 
-            text: 'Search',
-            onPressed: () => Navigator.pushReplacement(context, routeDukascango(page: SearchClientScreen())),
+            _ItemButton(
+              i: 1,
+              index: index,
+              iconData: Icons.search,
+              text: 'Search',
+              onPressed: () => Navigator.pushReplacement(
+                  context, routeDukascango(page: SearchClientScreen())),
             ),
-          _ItemButton(
-            i: 2, 
-            index: index, 
-            iconData: Icons.qr_code_scanner,
-            text: 'Scan & Go',
-            onPressed: () => Navigator.push(context, routeDukascango(page: StoreCheckinScreen())),
+            _ItemButton(
+              i: 2,
+              index: index,
+              iconData: Icons.qr_code_scanner,
+              text: 'Scan & Go',
+              onPressed: () => Navigator.push(
+                  context, routeDukascango(page: StoreCheckinScreen())),
             ),
-          _ItemButton(
-            i: 3,
-            index: index,
-            iconData: Icons.local_mall_outlined, 
-            text: 'Cart',
-            onPressed: () => Navigator.pushReplacement(context, routeDukascango(page: CartClientScreen())),
-          ),
-          _ItemButton(
-            i: 4,
-            index: index, 
-            iconData: Icons.person_outline_outlined, 
-            text: 'Profile',
-            onPressed: () => Navigator.pushReplacement(context, routeDukascango(page: ProfileClientScreen())),
+            _ItemButton(
+              i: 3,
+              index: index,
+              iconData: Icons.local_mall_outlined,
+              text: 'Cart',
+              onPressed: () => Navigator.pushReplacement(
+                  context, routeDukascango(page: CartClientScreen())),
             ),
-        ],
-      )
-    );
+            _ItemButton(
+              i: 4,
+              index: index,
+              iconData: Icons.person_outline_outlined,
+              text: 'Profile',
+              onPressed: () => Navigator.pushReplacement(
+                  context, routeDukascango(page: ProfileClientScreen())),
+            ),
+          ],
+        ));
   }
 }
 
 class _ItemButton extends StatelessWidget {
-  
   final int i;
   final int index;
   final IconData iconData;
   final String text;
   final VoidCallback? onPressed;
 
-  const _ItemButton({ required this.i, required this.index, required this.iconData, required this.text, this.onPressed });
+  const _ItemButton(
+      {required this.i,
+      required this.index,
+      required this.iconData,
+      required this.text,
+      this.onPressed});
 
   @override
   Widget build(BuildContext context) {
@@ -86,18 +89,23 @@ class _ItemButton extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 7.0),
         decoration: BoxDecoration(
-          color: ( i == index ) ? ColorsDukascango.primaryColor.withOpacity(.9) : Colors.transparent,
-          borderRadius: BorderRadius.circular(15.0)
-        ),
-        child: ( i == index ) 
-          ? Row(
-            children: [
-                Icon(iconData, color: Colors.white, size: 25),
-                const SizedBox(width: 6.0),
-                TextCustom(text: text, fontSize: 17, color: Colors.white, fontWeight: FontWeight.w500 )
-              ],
-            )
-          :  Icon(iconData, size: 28),
+            color: (i == index)
+                ? ColorsDukascango.primaryColor.withOpacity(.9)
+                : Colors.transparent,
+            borderRadius: BorderRadius.circular(15.0)),
+        child: (i == index)
+            ? Row(
+                children: [
+                  Icon(iconData, color: Colors.white, size: 25),
+                  const SizedBox(width: 6.0),
+                  TextCustom(
+                      text: text,
+                      fontSize: 17,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500)
+                ],
+              )
+            : Icon(iconData, size: 28),
       ),
     );
   }
--- lib/presentation/screens/admin/category/add_category_admin_screen.dart
+++ lib/presentation/screens/admin/category/add_category_admin_screen.dart
@@ -5,16 +5,14 @@ import 'package:dukascango/domain/bloc/blocs.dart';
 import 'package:dukascango/presentation/components/components.dart';
 import 'package:dukascango/presentation/helpers/helpers.dart';
 import 'package:dukascango/presentation/screens/admin/category/categories_admin_screen.dart';
-import 'package:dukascango/presentation/themes/colors_frave.dart';
+import 'package:dukascango/presentation/themes/colors_dukascango.dart';
 
 class AddCategoryAdminScreen extends StatefulWidget {
-  
   @override
   _AddCategoryAdminScreenState createState() => _AddCategoryAdminScreenState();
 }
 
 class _AddCategoryAdminScreenState extends State<AddCategoryAdminScreen> {
-
   late TextEditingController _nameCategoryController;
   late TextEditingController _categoryDescriptionController;
 
@@ -38,22 +36,24 @@ class _AddCategoryAdminScreenState extends State<AddCategoryAdminScreen> {
 
   @override
   Widget build(BuildContext context) {
-
     final productBloc = BlocProvider.of<ProductsBloc>(context);
 
     return BlocListener<ProductsBloc, ProductsState>(
       listener: (context, state) {
-        if( state is LoadingProductsState ){
+        if (state is LoadingProductsState) {
           modalLoading(context);
         }
-        if(state is SuccessProductsState ) {
+        if (state is SuccessProductsState) {
           Navigator.pop(context);
-          Navigator.pushReplacement(context, routeFrave(page: CategoriesAdminScreen()));
+          Navigator.pushReplacement(
+              context, routeFrave(page: CategoriesAdminScreen()));
         }
-        if( state is FailureProductsState ){
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: TextCustom(text: state.error, color: Colors.white), backgroundColor: Colors.red ));
+        if (state is FailureProductsState) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: TextCustom(text: state.error, color: Colors.white),
+              backgroundColor: Colors.red));
         }
-      } ,
+      },
       child: Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
@@ -66,42 +66,50 @@ class _AddCategoryAdminScreenState extends State<AddCategoryAdminScreen> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: const [
-                Icon(Icons.arrow_back_ios_new_rounded, color: ColorsFrave.primaryColor , size: 17),
-                TextCustom(text: 'Back', fontSize: 17, color: ColorsFrave.primaryColor )
+                Icon(Icons.arrow_back_ios_new_rounded,
+                    color: ColorsDukascango.primaryColor, size: 17),
+                TextCustom(
+                    text: 'Back',
+                    fontSize: 17,
+                    color: ColorsDukascango.primaryColor)
               ],
             ),
           ),
           elevation: 0,
           actions: [
             TextButton(
-              onPressed: () {
-                if( _keyForm.currentState!.validate() ){
-                  productBloc.add(OnAddNewCategoryEvent(_nameCategoryController.text, _categoryDescriptionController.text));
-                }
-              }, 
-              child: const TextCustom(text: 'Save', color: ColorsFrave.primaryColor )
-            )
+                onPressed: () {
+                  if (_keyForm.currentState!.validate()) {
+                    productBloc.add(OnAddNewCategoryEvent(
+                        _nameCategoryController.text,
+                        _categoryDescriptionController.text));
+                  }
+                },
+                child: const TextCustom(
+                    text: 'Save', color: ColorsDukascango.primaryColor))
           ],
         ),
         body: Form(
           key: _keyForm,
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const SizedBox(height: 20.0),
                 const TextCustom(text: 'Category name'),
                 const SizedBox(height: 5.0),
-                FormFieldFrave(
+                FormFieldDukascango(
                   controller: _nameCategoryController,
                   hintText: 'Drinks',
-                  validator: RequiredValidator(errorText: 'Category name is required'),
+                  validator:
+                      RequiredValidator(errorText: 'Category name is required'),
                 ),
                 const SizedBox(height: 25.0),
                 const TextCustom(text: 'Category Description'),
                 const SizedBox(height: 5.0),
-                FormFieldFrave(
+                FormFieldDukascango(
                   controller: _categoryDescriptionController,
                   maxLine: 8,
                 )
